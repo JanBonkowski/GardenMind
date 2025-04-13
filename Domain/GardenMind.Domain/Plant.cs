c0b67c563a4c17d8ceb7753d43e69bc82e94dad4
Jan Bońkowski <devbonkowski@gmail.com>
2025-04-13 12:26:43 +0200

    Added basic implementation of Plant

---
+++ Domain/GardenMind.Domain/Plant.cs
@@ -0,0 +1,58 @@
+﻿using GardenMind.Domain.Seasons;
+using GardenMind.Domain.Seasons.Exceptions;
+
+namespace GardenMind.Domain
+{
+    public class Plant
+    {
+        public int Id { get; init; }
+        public Guid Tag { get; init; }
+        public Season Season { get; init; } = default;
+        public DateTime PlantedAt { get; init; }
+        public Species Species { get; init; } = default;
+        public string? Genus { get; init; }
+
+        private Plant()
+        {
+        }
+
+        public static Plant Create(Guid tag,
+                                   Season season,
+                                   Species species,
+                                   DateTime plantedAt,
+                                   string? genus = null,
+                                   string? photoUri = null)
+        {
+            ArgumentNullException.ThrowIfNull(season, nameof(season));
+            ArgumentNullException.ThrowIfNull(species, nameof(species));
+
+            var tagIsEmpty = Guid.Empty.Equals(tag);
+            if (tagIsEmpty)
+            {
+                throw new ArgumentNullException(nameof(tag));
+            }
+
+            if (season.Terminated())
+            {
+                throw new SeasonAlreadyTerminatedException();
+            }
+
+            var dateWasNotSet = DateTime.MinValue.Equals(plantedAt);
+            if (dateWasNotSet)
+            {
+                throw new ArgumentOutOfRangeException(nameof(plantedAt));
+            }
+
+            var plant = new Plant
+            {
+                Genus = genus,
+                Tag = tag,
+                PlantedAt = plantedAt,
+                Season = season,
+                Species = species
+            };
+
+            return plant;
+        }
+    }
+}
