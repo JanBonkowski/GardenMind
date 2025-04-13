7d4f58a23b6a39934fde66083f5e493664435dee
Jan Bońkowski <devbonkowski@gmail.com>
2025-04-13 12:20:29 +0200

    GAR-9: Added `Species` entity and tests (#7)
    * Added Bogus package and implemented basic unit tests
    
    * Added Species entity

---
+++ Domain/GardenMind.Domain/Species.cs
@@ -0,0 +1,37 @@
+﻿namespace GardenMind.Domain;
+
+public class Species
+{
+    public const int MAX_NAME_LENGTH = 128;
+    public const int MIN_NAME_LENGTH = 3;
+
+    public int Id { get; private set; } = default;
+
+    public string Name { get; private set; } = string.Empty;
+
+    private Species()
+    {
+    }
+
+    public static Species Create(string name)
+    {
+        name = name.Trim();
+
+        ArgumentException.ThrowIfNullOrWhiteSpace(name, nameof(name));
+
+        if (name.Length > MAX_NAME_LENGTH)
+        {
+            throw new ArgumentException($"Species name must not exceed {MAX_NAME_LENGTH} characters.", nameof(name));
+        }
+
+        if (name.Length < MIN_NAME_LENGTH)
+        {
+            throw new ArgumentException($"Species name must be longer than {MIN_NAME_LENGTH} characters.", nameof(name));
+        }
+
+        return new Species
+        {
+            Name = name
+        };
+    }
+}
