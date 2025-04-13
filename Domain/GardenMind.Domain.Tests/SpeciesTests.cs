f2deae77632508b201ca583747b73883a4e35629
Jan Bońkowski <devbonkowski@gmail.com>
2025-04-13 12:18:06 +0200

    Added Bogus package and implemented basic unit tests

---
+++ Domain/GardenMind.Domain.Tests/SpeciesTests.cs
@@ -0,0 +1,52 @@
+﻿using Bogus;
+using FluentAssertions;
+
+namespace GardenMind.Domain.Tests;
+
+public class SpeciesTests
+{
+    private static Faker faker = new();
+
+    [Test]
+    [TestCaseSource(nameof(ValidSpeciesNames))]
+    public void Creating_New_Species_Succeeds_For_Valid_Name(string name)
+    {
+        // given
+        var speciesName = name.Trim();
+
+        // when
+        var species = Species.Create(speciesName);
+
+        // then
+        species.Name.Should().Be(speciesName);
+    }
+
+    [Test]
+    [TestCaseSource(nameof(InvalidSpeciesNames))]
+    public void Creating_New_Species_Throws_For_Invalid_Name(string name)
+    {
+        // when
+        Action createNewSpecies = () => Species.Create(name);
+
+        // then
+        createNewSpecies.Should().Throw<ArgumentException>();
+    }
+
+    public static IEnumerable<string> InvalidSpeciesNames()
+    {
+        var tooLongName = faker.Random.AlphaNumeric(129);
+
+        yield return tooLongName;
+        yield return "\t\t\t\t\t";
+        yield return "\n\n\n\n\n\n\n";
+        yield return "\r\n\r\n\r\n\r\n\r\n";
+    }
+
+    public static IEnumerable<string> ValidSpeciesNames()
+    {
+        var longName = faker.Random.AlphaNumeric(128);
+        yield return "abc";
+        yield return longName.ToString();
+        yield return $"\t\t\t\n\n\n\n\r\r\r\r{faker.Name.FullName}";
+    }
+}
