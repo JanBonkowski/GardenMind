17924c1f009a7bcc0bd4840d3d60f58f5f6aeaf6
Jan Bońkowski <devbonkowski@gmail.com>
2025-04-13 12:15:55 +0200

    GAR-8: Added `Seasons` entity and tests" (#6)
    * Added FluentAssertions and implemented basic tests for seasons
    
    * Added basic entity describing Season and tests

---
+++ Domain/GardenMind.Domain.Tests/SeasonTests.cs
@@ -0,0 +1,121 @@
+﻿using FluentAssertions;
+using GardenMind.Domain.Seasons;
+using GardenMind.Domain.Seasons.Exceptions;
+
+namespace GardenMind.Domain.Tests;
+
+public class SeasonTests
+{
+    [Test]
+    public void Creating_New_Season_Succeeds()
+    {
+        // when
+        Action createSeason = () => Season.Create();
+
+        // then
+        Assert.DoesNotThrow(() => createSeason());
+    }
+
+    [Test]
+    public void New_Season_Has_Created_Date_And_Is_Planned()
+    {
+        // given
+        var season = Season.Create();
+
+        // then
+        season.Status.Should().Be(SeasonStatus.Planned);
+        season.CreatedAt.Should().NotBe(DateTime.MinValue);
+        season.StartedAt.Should().BeNull();
+        season.TerminatedAt.Should().BeNull();
+    }
+
+    [Test]
+    public void Starting_Season_Sets_Status_To_Started()
+    {
+        // given
+        var season = Season.Create();
+
+        // when
+        season.Start();
+
+        // then
+        season.Started().Should().BeTrue();
+        season.Status.Should().Be(SeasonStatus.Started);
+        season.StartedAt.Should().BeCloseTo(DateTime.Now, TimeSpan.FromSeconds(10));
+        season.TerminatedAt.Should().BeNull();
+    }
+
+    [Test]
+    public void Starting_Season_Should_Throw_For_Already_Started_Season()
+    {
+        // given
+        var season = Season.Create();
+        season.Start();
+
+        // when
+        Action starting = () => season.Start();
+
+        // then
+        starting.Should().Throw<SeasonAlreadyStartedException>();
+    }
+
+    [Test]
+    public void Starting_Season_Should_Throw_For_Already_Terminated_Season()
+    {
+        // given
+        var season = Season.Create();
+        season.Start();
+        season.Terminate();
+
+        // when
+        Action starting = () => season.Start();
+
+        // then
+        starting.Should().Throw<SeasonAlreadyTerminatedException>();
+    }
+
+    [Test]
+    public void Terminating_New_Season_Should_Throw()
+    {
+        // given
+        var season = Season.Create();
+
+        // when
+        Action termination = () => season.Terminate();
+
+        // then
+        termination.Should().Throw<SeasonNotStartedException>();
+    }
+
+    [Test]
+    public void Terminating_Terminated_Season_Should_Throw()
+    {
+        // given
+        var season = Season.Create();
+        season.Start();
+        season.Terminate();
+
+        // when
+        Action termination = () => season.Terminate();
+
+        // then
+        termination.Should().Throw<SeasonAlreadyTerminatedException>();
+    }
+
+    [Test]
+    public void Terminating_Started_Season_Should_Succeed()
+    {
+        // given
+        var season = Season.Create();
+        season.Start();
+
+        // when
+        season.Terminate();
+
+        // then
+        season.Terminated().Should().BeTrue();
+        season.Status.Should().Be(SeasonStatus.Terminated);
+        season.StartedAt.Should().NotBeNull();
+        season.TerminatedAt.Should().BeCloseTo(DateTime.Now, TimeSpan.FromSeconds(10));
+    }
+}
---
+++ Domain/GardenMind.Domain/Seasons/Season.cs
@@ -0,0 +1,69 @@
+﻿using GardenMind.Domain.Seasons.Exceptions;
+
+namespace GardenMind.Domain.Seasons;
+
+public partial class Season
+{
+    public int Id { get; private set; } = default;
+    public DateTime CreatedAt { get; private set; }
+    public DateTime? StartedAt { get; private set; }
+    public DateTime? TerminatedAt { get; private set; }
+
+    public static Season Create()
+    {
+        return new Season() { CreatedAt = DateTime.Now };
+    }
+
+    public void Start()
+    {
+        if (Started())
+        {
+            throw new SeasonAlreadyStartedException();
+        }
+
+        if (Terminated())
+        {
+            throw new SeasonAlreadyTerminatedException();
+        }
+
+        StartedAt = DateTime.Now;
+    }
+
+    public bool Started() => StartedAt.HasValue && !TerminatedAt.HasValue;
+
+    public bool Terminated() => TerminatedAt.HasValue;
+
+    public void Terminate()
+    {
+        if (Terminated())
+        {
+            throw new SeasonAlreadyTerminatedException();
+        }
+
+        if (!Started())
+        {
+            throw new SeasonNotStartedException();
+        }
+
+
+        TerminatedAt = DateTime.Now;
+    }
+
+    public SeasonStatus Status
+    {
+        get
+        {
+            if (!Started() && !Terminated())
+            {
+                return SeasonStatus.Planned;
+            }
+
+            if (Started() && !Terminated())
+            {
+                return SeasonStatus.Started;
+            }
+
+            return SeasonStatus.Terminated;
+        }
+    }
+}
