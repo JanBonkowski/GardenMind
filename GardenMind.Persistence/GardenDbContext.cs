3f1da0c4cd87460238df19c04b839ff5aadeedec
Jan Bońkowski <devbonkowski@gmail.com>
2025-04-15 10:36:05 +0200

    Added basic configuration for Species entity

---
+++ GardenMind.Persistence/GardenDbContext.cs
@@ -0,0 +1,25 @@
+﻿using GardenMind.Domain;
+using GardenMind.Persistence.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace GardenMind.Persistence;
+
+public class GardenDbContext : DbContext
+{
+    public DbSet<Species> Species { get; set; }
+
+    public GardenDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected GardenDbContext()
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfiguration(new SpeciesConfiguration());
+    }
+}
