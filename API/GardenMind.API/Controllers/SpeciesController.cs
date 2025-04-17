e46b55062c1e00dead054d3661d9e2a8b35fd704
Jan Bońkowski <devbonkowski@gmail.com>
2025-04-17 14:42:46 +0200

    Added basic controller for Species management

---
+++ API/GardenMind.API/Controllers/SpeciesController.cs
@@ -0,0 +1,27 @@
+﻿using GardenMind.Services.Species;
+using GardenMind.Services.Species.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GardenMind.API.Controllers
+{
+    [ApiController]
+    [Route("api/species")]
+    public class SpeciesController : ControllerBase
+    {
+        private readonly ILogger<SpeciesController> _logger;
+        private readonly QuerySpecies _querySpecies;
+
+        public SpeciesController(ILogger<SpeciesController> logger, QuerySpecies querySpecies)
+        {
+            _logger = logger;
+            _querySpecies = querySpecies;
+        }
+
+        [HttpGet]
+        public async Task<SpeciesListResponse> GetSpeciesList(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Attempting to retrieve list of species in application");
+            return await _querySpecies.GetAll(cancellationToken);
+        }
+    }
+}
