ddf07f9bd68e25e747f05996141868745b89f309
Jan Bońkowski <devbonkowski@gmail.com>
2025-04-17 10:36:16 +0200

    GAR-13: Retrieve database connection options from AWS Parameter Store (#15)

--- API/GardenMind.API/Program.cs
+++ API/GardenMind.API/Program.cs
@@ -1,12 +1,35 @@
 using GardenMind.Persistence;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<GardenDbContext>(x =>
+builder.Configuration.AddSystemsManager(ssm =>
 {
-    x.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlDatabase"));
+    ssm.Path = $"/GardenMind/{builder.Environment.EnvironmentName}";
+    ssm.AwsOptions = new Amazon.Extensions.NETCore.Setup.AWSOptions()
+    {
+        Profile = "default",
+        Region = Amazon.RegionEndpoint.EUNorth1
+    };
+});
+
+builder.Services.Configure<DatabaseOptions>(builder.Configuration.GetSection(DatabaseOptions.SectionName));
+
+builder.Services.AddDbContext<GardenDbContext>((services, ctx) =>
+{
+    var config = services.GetRequiredService<IOptions<DatabaseOptions>>().Value;
+    var connectionStringBuilder = new NpgsqlConnectionStringBuilder()
+    {
+        Host = config.Host,
+        Database = config.Database,
+        Username = config.Username,
+        Password = config.Password,
+        Port = config.Port
+    };
+    ctx.UseNpgsql(connectionStringBuilder.ConnectionString);
 });
 
 builder.Services.AddControllers();
@@ -14,6 +37,11 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+app.MapGet("/", async (GardenDbContext ctx) =>
+{
+    return await ctx.Species.ToListAsync();
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
