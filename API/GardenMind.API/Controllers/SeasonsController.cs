6792aaa643eb6a2e9737723cbbcec1d14e54035d
Jan Bońkowski <devbonkowski@gmail.com>
2025-04-18 14:28:07 +0200

    GAR-23: Ability to create new Season (#19)
    * Added endpoint to create Seasons, along with service and integration test
    
    * Added unit tests for `SeasonsCreator`
    
    * Split `GardeningTestingUtils` to smaller helper classes
    
    * Fixed failing integration tests

---
+++ API/GardenMind.API/Controllers/SeasonsController.cs
@@ -0,0 +1,31 @@
+﻿using GardenMind.Services.Seasons;
+using GardenMind.Services.Seasons.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GardenMind.API.Controllers
+{
+    [ApiController]
+    [Route("api/seasons")]
+    public class SeasonsController : ControllerBase
+    {
+        private readonly ILogger<SeasonsController> _logger;
+        private readonly SeasonCreator _seasonCreator;
+
+        public SeasonsController(
+            ILogger<SeasonsController> logger,
+            SeasonCreator seasonCreator)
+        {
+            _logger = logger;
+            _seasonCreator = seasonCreator;
+        }
+
+        [HttpPost]
+        public async Task<CreatedResult> CreateSeason([FromBody]CreateSeasonRequest request, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Attempting to create a new season");
+            var newSeasonId = await _seasonCreator.Create(request, cancellationToken);
+
+            return Created("api/seasons/", newSeasonId);
+        }
+    }
+}
---
+++ Services/GardenMind.Services.Tests/Seasons/SeasonsCreatorTests.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using GardenMind.Domain.Seasons;
+using GardenMind.Persistence;
+using GardenMind.Services.Seasons;
+using GardenMind.Services.Seasons.Models;
+using Moq;
+using Moq.EntityFrameworkCore;
+
+namespace GardenMind.Services.Tests.Seasons
+{
+    [TestFixture]
+    public class SeasonsCreatorTests
+    {
+        private Mock<GardenDbContext> _ctx;
+        private SeasonCreator _sut;
+
+        private List<Season> _seasons = [];
+
+        [SetUp]
+        public void Setup()
+        {
+            _seasons = [];
+            _ctx = new Mock<GardenDbContext>();
+            _ctx.Setup(x => x.Seasons).ReturnsDbSet(_seasons);
+            _ctx.Setup(x => x.Seasons.Add(It.IsAny<Season>())).Callback<Season>((entry) =>
+            {
+                _seasons.Add(entry);
+            });
+            _ctx.Setup(x => x.SaveChangesAsync(It.IsAny<CancellationToken>())).Returns(() =>
+            {
+                var insertedSeason = _ctx.Object.Seasons.First();
+                var field = typeof(Season).GetProperty(nameof(Season.Id));
+                field!.SetValue(insertedSeason, Random.Shared.Next(0, 100));
+
+                return Task.FromResult(1);
+            });
+
+            _sut = new SeasonCreator(_ctx.Object);
+        }
+
+        [TestCaseSource(nameof(SeasonStatusByRequirement))]
+        public async Task Creates_Seasons_Successfully((bool shouldBeStarted, SeasonStatus expectedStatus) expectedData)
+        {
+            // given
+            var request = new CreateSeasonRequest(expectedData.shouldBeStarted);
+
+            // when
+            var createdId = await _sut.Create(request);
+
+            // then
+            createdId.Should().BeGreaterThan(0);
+            var createdSeason = _ctx.Object.Seasons.First();
+            createdSeason.Status.Should().Be(expectedData.expectedStatus);
+        }
+
+        public static IEnumerable<(bool, SeasonStatus)> SeasonStatusByRequirement()
+        {
+            yield return (false, SeasonStatus.Planned);
+            yield return (true, SeasonStatus.Started);
+        }
+    }
+}
