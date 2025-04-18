e4c873204e9c00c4ca8f5830b3c14b44d6efab05
Jan Bońkowski <devbonkowski@gmail.com>
2025-04-18 13:44:57 +0200

    Split `GardeningTestingUtils` to smaller helper classes

---
+++ GardenMind.SharedTest/PlantsTestingUtils.cs
@@ -0,0 +1,15 @@
+﻿using GardenMind.Domain.Plants;
+
+namespace GardenMind.SharedTest;
+
+public class PlantsTestingUtils
+{
+    public static Plant NewPlant()
+    {
+        var tag = Guid.NewGuid();
+        var species = SpeciesTestingUtils.NewSpecies();
+        var season = SeasonsTestingUtils.NewPlannedSeason();
+
+        return Plant.Create(tag, season, species, DateTime.UtcNow);
+    }
+}
