9aeabf301eba73b3ccfce9d79fdf424b78309a0b
Jan Bońkowski <devbonkowski@gmail.com>
2025-04-18 15:22:57 +0200

    GAR-23: Added possibility to start season (#20)

---
+++ Domain/GardenMind.Domain/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace GardenMind.Domain.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException(string name) : base($"Could not find {name} with specified id in database")
+        {
+        }
+    }
+}
