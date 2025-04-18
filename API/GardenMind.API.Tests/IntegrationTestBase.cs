adce5d1a640ce77673e2911504f263ea7dd4f70d
Jan Bońkowski <devbonkowski@gmail.com>
2025-04-18 12:23:44 +0200

    GAR-22: Added integration test project (#17)
    * GAR-22: Added integration test project
    
    * Temporarily turn off GH Actions for int tests

---
+++ API/GardenMind.API.Tests/IntegrationTestBase.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace GardenMind.API.Tests
+{
+    public abstract class IntegrationTestBase
+    {
+        private readonly GardenWebApplicationFactory _factory;
+        private readonly WebApplicationFactoryClientOptions _options;
+
+        protected IntegrationTestBase()
+        {
+            _factory = new GardenWebApplicationFactory();
+            _options = new WebApplicationFactoryClientOptions()
+            {
+                BaseAddress = new Uri("https://localhost"),
+                AllowAutoRedirect = true
+            };
+        }
+
+        public HttpClient Client => _factory.CreateClient(_options);
+
+        
+    }
+}
---
+++ API/GardenMind.API.Tests/Tests/SpeciesIntegrationTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using GardenMind.Shared.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+
+namespace GardenMind.API.Tests.Tests
+{
+    public class SpeciesIntegrationTests : IntegrationTestBase
+    {
+        [TestCaseSource(nameof(PageQueryStringGenerator))]
+        public async Task ListSpecies(string? queryString)
+        {
+            //given
+            var client = Client;
+            client.DefaultRequestHeaders.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("application/json"));
+
+            // when
+            var response = await client.GetAsync($"api/species{queryString}");
+
+            // then
+            response.EnsureSuccessStatusCode();
+        }
+
+        private static IEnumerable<string?> PageQueryStringGenerator()
+        {
+
+            yield return QueryString.Create(
+            [
+                new KeyValuePair<string, string?>(nameof(PageRequest.PageNumber), "0"),
+                new KeyValuePair<string, string?>(nameof(PageRequest.PageSize), "3")
+            ]).Value;
+
+            yield return QueryString.Create(new List<KeyValuePair<string, string?>>()).Value;
+        }
+    }
+}
